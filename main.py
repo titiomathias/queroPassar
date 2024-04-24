d39096e5da6d9c702788332c199356230a757e28
Matheus de Alencar <matheus.alencar@leucotron.com.br>
2024-04-24 09:28:32 -0300

    New source

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title = "Minha primeira página!"
+    page.title = "Home"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    page.add(ft.SafeArea(ft.Text("Olá, moça.")))
+    page.add(
+        ft.Row(
+            [
+                ft.TextField(label="Adicionar Matéria", hint_text="Digite o nome da matéria aqui..."), ft.FilledButton(text="Adicionar", icon="add")
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+   )
+
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
 
 
 ft.app(main)
