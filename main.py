8528bc4a331c83cfd3d0712cb06aa1035698949e
Matheus de Alencar <matheus.alencar@leucotron.com.br>
2024-04-24 09:58:07 -0300

    New source

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ def main(page: ft.Page):
     page.title = "Home"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    page.add(
-        ft.Row(
-            [
-                ft.TextField(label="Adicionar Matéria", hint_text="Digite o nome da matéria aqui..."), ft.FilledButton(text="Adicionar", icon="add")
+    def add_course(e):
+        courses_view.controls.append(ft.TextField(label=new_course.value))
+        new_course.value = ''
+        view.update()
+
+    new_course = ft.TextField(label="Adicionar Matéria", hint_text="Digite o nome da matéria aqui...", expand=True)
+    courses_view = ft.Column()
+    view = ft.Column(
+        width=600,
+        controls=[
+            ft.Row(controls=[
+                new_course,
+                ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_course)
             ],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
-   )
+            ),
+            courses_view,
+        ],
+    )
 
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.add(view)
 
 
 ft.app(main)
